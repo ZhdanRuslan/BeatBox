ebee0c3c466eb2222efd10542d4da0f37600a774
Ruslan <zhd.ruslan@gmail.com>
2017-11-28 08:43:19 +0200

    [add storage for sounds]

--- app/src/main/java/com/zhdan/beatbox/BeatBox.java
+++ app/src/main/java/com/zhdan/beatbox/BeatBox.java
@@ -5,12 +5,16 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
 
     private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+
     public BeatBox(Context context) {
         mAssets = context.getAssets();
         loadSounds();
