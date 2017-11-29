4a794305c93b98a49ea444815b52fd918d4ab611
Ruslan <zhd.ruslan@gmail.com>
2017-11-29 08:51:58 +0200

    [get/set sound id]

--- app/src/main/java/com/zhdan/beatbox/Sound.java
+++ app/src/main/java/com/zhdan/beatbox/Sound.java
@@ -3,6 +3,7 @@ package com.zhdan.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -17,5 +18,12 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
