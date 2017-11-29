705bf0784a892ffe42769473d089a9d94d09bd82
Ruslan <zhd.ruslan@gmail.com>
2017-11-29 08:59:12 +0200

    [impl onClick]

--- app/src/main/java/com/zhdan/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/zhdan/beatbox/BeatBoxFragment.java
@@ -34,7 +34,7 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private Button mButton;
         private  Sound mSound;
@@ -42,11 +42,17 @@ public class BeatBoxFragment extends Fragment {
         public SoundHolder(LayoutInflater inflater, ViewGroup countainer) {
             super(inflater.inflate(R.layout.list_item_sound, countainer, false));
             mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound){
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+        
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
 
